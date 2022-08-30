a44707542e9388bad6fffd58eabb02fe5e6fe41a
vishal035 <kumarvishal880288@gmail.com>
2022-08-30 18:49:33 +0530

    Priject Setup and Configured ⚡

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter'],
+      },
+      colors: {
+        primary: '#654DE4',
+        'primary-black': '#242424',
+        'primary-gray': '#525A63',
+        'primary-gray-light': '#B7BAC2',
+      },
+    },
+  },
+  plugins: [],
+};
