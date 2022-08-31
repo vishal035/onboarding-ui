c5de60d0326fb6f8b0aaa15d88f15b2a947424b7
vishal035 <kumarvishal880288@gmail.com>
2022-08-31 12:36:30 +0530

    Step Progress Bar Styling Fixed

--- src/components/Header/Navigation/StepCount.jsx
+++ src/components/Header/Navigation/StepCount.jsx
@@ -4,14 +4,32 @@ import OnboardContext from '../../../store/onboard';
 const StepCount = () => {
   /** using Context Hook */
   const ctx = useContext(OnboardContext);
+  console.log(`${25 * ctx.onStep}%`);
 
   return (
     <div>
       <h2 className="sr-only">Steps</h2>
 
-      <div className="relative after:inset-x-0 after:h-0.5 after:absolute after:top-1/2 after:-translate-y-1/2 after:block after:rounded-lg after:bg-gray-100">
-        <ol className="relative z-10 flex justify-between text-base font-medium text-primary-black">
-          <li className="flex items-center p-2 bg-white">
+      {/* <div className="relative after:inset-x-0 after:h-0.5 after:absolute after:top-1/2 after:-translate-y-1/2 after:block after:rounded-lg after:bg-primary-gray-light/30"> */}
+      <div className="relative ">
+        <div className="absolute top-6 left-4 w-11/12 bg-gray-200 rounded-full h-0.5 mb-4 dark:bg-gray-100">
+          <div
+            className="bg-primary h-0.5 rounded-full dark:bg-primary transition-all ease-in-out duration-500"
+            style={{
+              width: `${
+                ctx.onStep === 1
+                  ? 20 * ctx.onStep
+                  : ctx.onStep === 2
+                  ? 20 * ctx.onStep + 10
+                  : ctx.onStep === 3
+                  ? 20 * ctx.onStep + 20
+                  : 100
+              }%`,
+            }}
+          ></div>
+        </div>
+        <ol className="relative z-10 flex justify-between text-base font-medium text-primary-black ">
+          <li className="flex items-center p-2 ">
             <span
               className={`w-8 h-8 text-base font-medium leading-8 border-[.5px] border-primary-gray-light text-center rounded-full ${
                 ctx.onStep >= 1
@@ -23,7 +41,7 @@ const StepCount = () => {
             </span>
           </li>
 
-          <li className="flex items-center p-2 bg-white">
+          <li className="flex items-center p-2 ">
             <span
               className={`w-8 h-8 text-base font-medium leading-8 text-center border-[.5px] border-primary-gray-light rounded-full ${
                 ctx.onStep >= 2
@@ -35,7 +53,7 @@ const StepCount = () => {
             </span>
           </li>
 
-          <li className="flex items-center p-2 bg-white">
+          <li className="flex items-center p-2 ">
             <span
               className={`w-8 h-8 text-base font-medium leading-8 text-center border-[.5px] border-primary-gray-light rounded-full ${
                 ctx.onStep >= 3
@@ -47,7 +65,7 @@ const StepCount = () => {
             </span>
           </li>
 
-          <li className="flex items-center p-2 bg-white">
+          <li className="flex items-center p-2 ">
             <span
               className={`w-8 h-8 text-base font-medium leading-8 text-center border-[.5px] border-primary-gray-light rounded-full ${
                 ctx.onStep >= 4
