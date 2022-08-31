bf8588e9d4feb55b90847223639f0af715483fa6
vishal035 <kumarvishal880288@gmail.com>
2022-08-31 11:02:12 +0530

    Onboarding Refatored

--- src/store/onboard.js
+++ src/store/onboard.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+/** Init OnboardContext */
 const OnboardContext = React.createContext({
   onStep: null,
   userOnboard: {
@@ -13,22 +14,27 @@ const OnboardContext = React.createContext({
   nextStep: () => {},
 });
 
+/** Defing methods and Properties */
 export const OnboardContextProvider = (props) => {
   const [onStep, setOnStep] = useState(1);
 
+  /** For Demo All Data will be Console Logged */
   const addUserHandler = (data) => {
     console.log(data);
   };
 
+  /** Reseting the Step from 4 ===> 1 */
   const resetSteps = () => {
     if (onStep === 4) setOnStep(1);
   };
 
+  /** Moving Formward from Current Step to next Step: 1(default) ==> 2(increment by +1) */
   const nextStepHandler = () => {
     if (onStep < 4) setOnStep((val) => val + 1);
     if (onStep === 4) resetSteps();
   };
 
+  /** Returning the OnboardContext Hook with data and methods */
   return (
     <OnboardContext.Provider
       value={{
