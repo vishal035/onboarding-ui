25109dfb33de7f2b98cc9e5c585b60b65861e0ea
vishal035 <kumarvishal880288@gmail.com>
2022-08-31 11:13:02 +0530

    Icon Color Change Based on Radio Select Value

--- src/components/Form/ThirdStepForm.jsx
+++ src/components/Form/ThirdStepForm.jsx
@@ -63,7 +63,11 @@ const ThirdStepForm = () => {
                 <div className="relative w-7 mb-2">
                   <svg preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
                     <path
-                      fill="currentColor"
+                      fill={`${
+                        workspaceFor === 'workspace-myselfy'
+                          ? '#654DE4'
+                          : 'currentColor'
+                      }`}
                       d="M7.5 6.5C7.5 8.981 9.519 11 12 11s4.5-2.019 4.5-4.5S14.481 2 12 2S7.5 4.019 7.5 6.5zM20 21h1v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h17z"
                     />
                   </svg>
@@ -92,7 +96,11 @@ const ThirdStepForm = () => {
                 <div className="relative w-7 mb-2">
                   <svg preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
                     <path
-                      fill="currentColor"
+                      fill={`${
+                        workspaceFor === 'workspace-team'
+                          ? '#654DE4'
+                          : 'currentColor'
+                      }`}
                       d="M12 11a5 5 0 0 1 5 5v6H7v-6a5 5 0 0 1 5-5zm-6.712 3.006a6.983 6.983 0 0 0-.28 1.65L5 16v6H2v-4.5a3.5 3.5 0 0 1 3.119-3.48l.17-.014zm13.424 0A3.501 3.501 0 0 1 22 17.5V22h-3v-6c0-.693-.1-1.362-.288-1.994zM5.5 8a2.5 2.5 0 1 1 0 5a2.5 2.5 0 0 1 0-5zm13 0a2.5 2.5 0 1 1 0 5a2.5 2.5 0 0 1 0-5zM12 2a4 4 0 1 1 0 8a4 4 0 0 1 0-8z"
                     />
                   </svg>
