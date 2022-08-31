cf8693ffb545c9631fe2c46edf5362e9c13de8c7
vishal035 <kumarvishal880288@gmail.com>
2022-08-31 13:08:38 +0530

    FONT SIZE

--- src/components/Form/SecondStepForm.jsx
+++ src/components/Form/SecondStepForm.jsx
@@ -25,9 +25,9 @@ const SecondStepForm = () => {
   };
 
   return (
-    <div className="px-4 py-16 mx-auto max-w-screen-xl sm:px-6 lg:px-6">
+    <div className="px-4 py-16 mx-auto max-w-screen-xl sm:px-6 lg:px-8">
       <div className="max-w-lg mx-auto text-center">
-        <h1 className="text-2xl font-bold sm:text-3xl text-primary-black">
+        <h1 className="text-2xl font-bold sm:text-2xl text-primary-black">
           Let's set up a home for all your work
         </h1>
 
